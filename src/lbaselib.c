1f9a44c5b9b8edcaa1322860cad9bbca2ec889d7
Tomás Hugo Almeida <tomas.hugo@gmail.com>
2022-03-12 22:23:44 -0300

    feat: print -> imprimir

--- src/lbaselib.c
+++ src/lbaselib.c
@@ -515,7 +515,7 @@ static const luaL_Reg base_funcs[] = {
   {"next", luaB_next},
   {"pairs", luaB_pairs},
   {"pcall", luaB_pcall},
-  {"print", luaB_print},
+  {"imprimir", luaB_print},
   {"warn", luaB_warn},
   {"rawequal", luaB_rawequal},
   {"rawlen", luaB_rawlen},
