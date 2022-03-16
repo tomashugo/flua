4975fd503ea53d6ae77dd6d09909544de3191f80
Tomás Hugo Almeida <tomas.hugo@gmail.com>
2022-03-15 21:37:47 -0300

    feat: tradução de mensagens de error de parsing

--- src/ldebug.c
+++ src/ldebug.c
@@ -722,7 +722,7 @@ static const char *varinfo (lua_State *L, const TValue *o) {
 static l_noret typeerror (lua_State *L, const TValue *o, const char *op,
                           const char *extra) {
   const char *t = luaT_objtypename(L, o);
-  luaG_runerror(L, "attempt to %s a %s value%s", op, t, extra);
+  luaG_runerror(L, "tentativa de %s para um %s value%s", op, t, extra);
 }
 
 
--- src/lvm.c
+++ src/lvm.c
@@ -738,7 +738,7 @@ lua_Integer luaV_idiv (lua_State *L, lua_Integer m, lua_Integer n) {
 lua_Integer luaV_mod (lua_State *L, lua_Integer m, lua_Integer n) {
   if (l_unlikely(l_castS2U(n) + 1u <= 1u)) {  /* special cases: -1 or 0 */
     if (n == 0)
-      luaG_runerror(L, "attempt to perform 'n%%0'");
+      luaG_runerror(L, "tentativa de executar 'n%%0'");
     return 0;   /* m % -1 == 0; avoid overflow with 0x80000...%-1 */
   }
   else {
